cee569c8c14b04ba690c1e1bcbc15dc889aad884
relesaku <sakuma@relegare.co.jp>
2026-08-15 21:11:58 +0900

    配色を Iron に変更（無彩色 + 白の強調）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-56a5256e', FONTS='wc-fonts';
+const V='wc-df38bbfe', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
