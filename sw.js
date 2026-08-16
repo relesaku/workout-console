690003d6a428ef292b627b38f80984e6f74fc173
relesaku <sakuma@relegare.co.jp>
2026-08-16 20:38:21 +0900

    週メニューを3ステップ（部位→種目→曜日）に

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-df38bbfe', FONTS='wc-fonts';
+const V='wc-5aed9f4e', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
