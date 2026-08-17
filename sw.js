d16d5afabe2b0dcabdb098a3f0473647844e3de3
relesaku <sakuma@relegare.co.jp>
2026-08-17 18:44:18 +0900

    今日タブの日付ストリップを週表示に

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-3e8c091d', FONTS='wc-fonts';
+const V='wc-dc1cfb4a', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
