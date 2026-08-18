1d57f160d6daee8f20d737be29c29851fb60fa20
relesaku <sakuma@relegare.co.jp>
2026-08-18 20:57:01 +0900

    メイン部位タグでセクション分け / 今日タブの行自動生成をやめる

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-46d32fc0', FONTS='wc-fonts';
+const V='wc-68b13c57', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
