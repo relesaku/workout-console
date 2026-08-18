c2ef854ae2cf99b06c6bf67418b4f6f0e36d129a
relesaku <sakuma@relegare.co.jp>
2026-08-18 17:06:08 +0900

    ジム候補の既定を空に

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-0d1dd40c', FONTS='wc-fonts';
+const V='wc-38eca9ba', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
