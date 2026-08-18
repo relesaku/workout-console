ec458e24ef878c978a1a97399294170e22035e18
relesaku <sakuma@relegare.co.jp>
2026-08-18 16:21:43 +0900

    目安から作るボタンを削除

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-2a375a62', FONTS='wc-fonts';
+const V='wc-06e69fb2', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
