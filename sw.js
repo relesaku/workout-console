11325e3d34300a971849d8981bdb0275bb4d8d90
relesaku <sakuma@relegare.co.jp>
2026-08-19 18:24:41 +0900

    自重種目は重量欄を出さない

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-f8b94a51', FONTS='wc-fonts';
+const V='wc-a3517e93', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
