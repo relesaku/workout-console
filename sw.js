31813004bce79d872eefc9f0ff535bc2e46c14af
relesaku <sakuma@relegare.co.jp>
2026-08-19 20:40:36 +0900

    行の完了チェックを廃止 / 完了は種目単位 / 追加ボタンを上へ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-ba1f1f19', FONTS='wc-fonts';
+const V='wc-fce9e3bd', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
