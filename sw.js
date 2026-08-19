4c2aaf4c67fdb3232e385cd61fb8168434b3cba5
relesaku <sakuma@relegare.co.jp>
2026-08-19 20:52:53 +0900

    完了は手動のみ / ボタンを分離

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-fce9e3bd', FONTS='wc-fonts';
+const V='wc-0c5ff970', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
