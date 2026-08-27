62034cd4dd9bc2852048d664c268c10a402cadfe
relesaku <sakuma@relegare.co.jp>
2026-08-27 17:04:30 +0900

    build 90757c33: 1RM推定式をEpley(÷30)からオコーナー式(÷40)へ変更(王道のRM換算表に合わせる)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-84b20afd', FONTS='wc-fonts';
+const V='wc-f80bb32b', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
