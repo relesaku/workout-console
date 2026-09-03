13127893716143f71a711480c5483006650b8279
relesaku <sakuma@relegare.co.jp>
2026-09-03 10:24:55 +0900

    build fc16cdcc: 履歴に週ごと推移グラフ+種目別記録(1RM推移)、設定/今日にバックアップ催促を追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wc-ef6ae971', FONTS='wc-fonts';
+const V='wc-7bc6b240', FONTS='wc-fonts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
